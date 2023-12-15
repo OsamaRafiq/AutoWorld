ac9452360a9dd9856e2f7c6e2cb00411bc22e0b3
OsamaRafiq <99024906+OsamaRafiq@users.noreply.github.com>
2023-12-16 00:28:13 +0500

    Add files via upload

---
+++ contact us/response.js
@@ -0,0 +1,21 @@
+const responseObj = {
+    hey: "Hey! Welcome to the AutoWorld how can i help",
+    help: "how can i help ?",
+    information : "You can explore 4 brands cars here",
+    thanks : "you are welcome",
+    bye : "Take care hope i was able to help",
+    brands : "yes 4 different brands",
+    founded : "AutoWorld was founded in 2004",
+    compare : "you can compare cars which fits you",
+    search : "you can search cars based on brand,price",
+
+
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    
+    };
+
+
+
+
+    
